8aa271e99d23c9d1596064dd2070dc1d5018e2f0
pedrovanzo <pedro_vanzo@hotmail.com>
2025-03-01 01:52:22 -0300

    Adds different theme options

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,9 @@ function App() {
     console.log(theme)
     setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'))
   }
+  const insertOrdinalTheme = (theme: any) => {
+    document.documentElement.setAttribute('data-theme-ordinal', theme)
+  }
   return (
     <>
       <nav className='mb-4'>
@@ -29,9 +32,28 @@ function App() {
           <li>options</li>
         </ul>
       </nav>
-      <button className='text-contrast bg-default p-2 rounded-md' onClick={toggleTheme}>
-        {theme === 'light' ? 'Dark' : 'Light'} Mode
-      </button>
+      <div className='my-2 flex flex-col gap-4 w-fit'>
+        <button className='p-2 rounded-md leading-none text-contrast bg-default' onClick={toggleTheme}>{theme === 'light' ? 'Dark' : 'Light'} Mode</button>
+        <button className='p-2 rounded-md leading-none bg-contrast text-default' onClick={()=>insertOrdinalTheme('null')}>Default Mode</button>
+        <button className='p-2 rounded-md leading-none bg-red-700 text-red-100' onClick={()=>insertOrdinalTheme('red')}>Red Mode</button>
+        <button className='p-2 rounded-md leading-none bg-blue-700 text-blue-100' onClick={()=>insertOrdinalTheme('blue')}>Blue Mode</button>
+        <button className='p-2 rounded-md leading-none bg-green-700 text-green-100' onClick={()=>insertOrdinalTheme('green')}>Green Mode</button>
+      </div>
+      <br />
+      <div className='w-60 bg-primary/15'>
+        <div className='w-full p-2 bg-primary'></div>
+        <div className='p-2 text-default'>Theme building</div>
+      </div>
+      <br />
+      <div className='w-60 bg-primary/15'>
+        <div className='w-full p-2 bg-primary'>
+        <div className='p-2 text-secondary'>Theme building</div>
+        </div>
+      </div>
+      <p className='text-default'>
+        Light and Dark mode changes between preset light and dark colors.<br />
+        Colored Modes add different color on elements that are able to have an ordinal color attributed to (primary, secondary...)
+      </p>
     </>
   )
 }
