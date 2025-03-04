3cd58cb622be581236ca55ba4c6898138ab15057
pedrovanzo <pedro_vanzo@hotmail.com>
2025-03-04 12:47:01 -0300

    fix environment variables

--- src/data/firebase.tsx
+++ src/data/firebase.tsx
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_apiKey,
-  authDomain: import.meta.env.authDomain,
-  projectId: import.meta.env.VITE_projectId,
-  storageBucket: import.meta.env.storageBucket,
-  messagingSenderId: import.meta.env.messagingSenderId,
-  appId: import.meta.env.appId,
-  measurementId: import.meta.env.measurementId,
+  apiKey: import.meta.env.DB_apiKey,
+  authDomain: import.meta.env.DB_authDomain,
+  projectId: import.meta.env.DB_projectId,
+  storageBucket: import.meta.env.DB_storageBucket,
+  messagingSenderId: import.meta.env.DB_messagingSenderId,
+  appId: import.meta.env.DB_appId,
+  measurementId: import.meta.env.DB_measurementId,
 };
 
 // Initialize Firebase
